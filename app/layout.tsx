7de80889431d7ec9abd2a3c6c20e09da51dd079a
Anil Ozsoy <anilozsoy@gmail.com>
2025-10-23 15:14:52 +0200

    Add Playfair font and update layout styling
    - Import Playfair font for serif typography
    - Add font variable to body className
    - Set background to black

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Playfair, Merriweather } from "next/font/google";
 import "./globals.css";
 import Navigation from "./components/Navigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const playfair = Playfair({
+  variable: "--font-serif",
   subsets: ["latin"],
 });
 
@@ -27,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${playfair.variable}  antialiased bg-black`}
       >
         <Navigation />
         {children}
