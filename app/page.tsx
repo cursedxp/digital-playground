1a3177e3997db9f4af5d821f7f144a474432512f
Anil Ozsoy <anilozsoy@gmail.com>
2025-11-03 13:03:07 +0100

    Update homepage to use new section components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import ComplencyCurve from "./components/ComplencyCurve";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import ServiceOverview from "./components/ServiceOverview";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <ServiceOverview />
+      <ComplencyCurve />
       <Footer />
     </>
   );
