7b7f853b98d6dbb9b20fccac14be2fd78bcd6c5d
Anil Ozsoy <anilozsoy@gmail.com>
2025-11-03 17:25:53 +0100

    Add ServicesDetailed and TechStack sections to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import ComplencyCurve from "./components/ComplencyCurve";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import ServiceOverview from "./components/ServiceOverview";
+import ServicesDetailed from "./components/ServicesDetailed";
+import TechStack from "./components/TechStack";
 
 export default function Home() {
   return (
@@ -9,6 +11,8 @@ export default function Home() {
       <Hero />
       <ServiceOverview />
       <ComplencyCurve />
+      <ServicesDetailed />
+      <TechStack />
       <Footer />
     </>
   );
