ec0e32c84eadb56902209b3f8fe3f6c7f27f4450
Anil Ozsoy <anilozsoy@gmail.com>
2025-11-04 11:39:36 +0100

    Add DeliveryProcess, Pricing, and FAQ sections to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import ComplencyCurve from "./components/ComplencyCurve";
+import DeliveryProcess from "./components/DeliveryProcess";
+import FAQ from "./components/FAQ";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
 import ServiceOverview from "./components/ServiceOverview";
 import ServicesDetailed from "./components/ServicesDetailed";
 import TechStack from "./components/TechStack";
@@ -12,7 +15,10 @@ export default function Home() {
       <ServiceOverview />
       <ComplencyCurve />
       <ServicesDetailed />
+      <DeliveryProcess />
       <TechStack />
+      <Pricing />
+      <FAQ />
       <Footer />
     </>
   );
